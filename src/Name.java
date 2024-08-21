82b9061ca27485eb5d59648d4b0f3bf0ff03f94c
Narayan Thakare <narayan111thakare@gmail.com>
2024-08-21 07:22:50 +0530

    first commit

---
+++ src/Name.java
@@ -0,0 +1,151 @@
+class Name
+{
+	public static void main(String[] args) 
+	{
+		
+		int n = 5 ;
+		///this is for N
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 || i==j||j==n-1   )
+							
+				{
+									System.out.print("**"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+		
+									System.out.println(" ");
+									
+	//this is for A
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 || i==0||i==n/2||j==n-1   )
+							
+				{
+									System.out.print("*"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+									System.out.println(" ");
+																System.out.println(" ");
+	
+	//this is for R
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 || i==0||j==n-1&&i<n/2  ||i==n/2 || i==j&&j>n/2)
+							
+				{
+									System.out.print("*"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+		System.out.println(" ");
+		
+		//this is for A
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 || i==0||i==n/2||j==n-1   )
+							
+				{
+									System.out.print("*"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+									System.out.println(" ");
+																System.out.println(" ");
+																
+																
+			//this is for y
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 && i<n/2 ||i ==n/2 || j ==n-1 || i==n-1 && j>n/2 )
+							
+				{
+									System.out.print("*"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+									System.out.println(" ");
+																System.out.println(" ");
+																
+		
+		//this is for A
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 || i==0||i==n/2||j==n-1   )
+							
+				{
+									System.out.print("*"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+									System.out.println(" ");
+																System.out.println(" ");
+																
+																
+		
+		///this is for N
+		for (int i = 0 ;i<n ;i++ )
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				if ( j==0 || i==j||j==n-1   )
+							
+				{
+									System.out.print("**"+" ");	
+				}
+				else{
+												System.out.print(" "+" ");
+				}
+			}
+							System.out.println(" ");
+			
+		}
+		
+									System.out.println(" ");
+
+	}
+}
